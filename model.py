9459983ba9d5d858887d9b172f54251c619f77eb
Luke Lopez <xlucas14@gmail.com>
2019-02-11 20:28:15 -0500

    Initial commit.

---
+++ model.py
@@ -0,0 +1,135 @@
+import openpyxl
+import re
+import time
+
+
+class Student:
+    def __init__(self):
+        self.name = None
+        self.booking = None
+        self.dates = []
+        self.column = None    # M, T, W, T, F, S, S
+        self.classes_per_day = [0, 0, 0, 0, 0, 0, 0]
+        self.class_dates_until_today = []
+
+
+class Model:
+    def __init__(self, file_path):
+        self.file_name = file_path
+
+        self.workbook = openpyxl.load_workbook(file_path)
+        self.sheet = self.workbook.active
+
+        self.col_row_regex = re.compile(r"<Cell '\S+'.([A-Z])(\d+)>")
+        self.date_format = "%m/%d/%Y"
+        self.alt_date_format = "%Y-%m-%d"
+        self.name_row = int("1")
+        self.booking_row = int("2")
+        self.schedcode_row = int("3")
+        self.dates_start_row = int("4")
+        self.ignore_column = "A"
+        self.students_list = []
+
+        self.process_sheet()
+        self.print_students()
+
+    def process_sheet(self):
+        self.read_sheet()
+        for student in self.students_list:
+            student.class_dates_until_today = self.calculate_class_days(student)
+
+    def read_sheet(self):
+        if len(self.students_list) > 1:
+            self.students_list.clear()
+        # Regex to scan the column and put the column in group 1, the row in group 2.
+        cols = self.sheet.columns
+
+        for col in cols:
+            # If the column is set to be skipped because it contains labels, skip it!
+            mo = self.col_row_regex.search(str(col))
+            current_column = mo.group(1)
+            if current_column == self.ignore_column:
+                continue
+
+            new_student = Student()
+            # Assign the new student a column if he doesn't have one.
+            if new_student.column is None:
+                new_student.column = mo.group(1)
+
+            for cell in col:
+                if cell.value is not None:
+                    mo = self.col_row_regex.search(str(cell))
+                    current_row = int(mo.group(2))
+                    if current_row == self.name_row:
+                        new_student.name = cell.value
+                    elif current_row == self.booking_row:
+                        new_student.booking = cell.value
+                    elif current_row == self.schedcode_row:
+                        new_student.classes_per_day = [int(i) for i in cell.value if i != "s"]
+                    elif current_row >= self.dates_start_row:
+                        year_month_day = str(cell.value)
+                        try:
+                            struct_time_date = time.strptime(year_month_day, self.date_format)
+                        except ValueError:
+                            struct_time_date = time.strptime(year_month_day, self.alt_date_format)
+                            print("Alt format used")
+                        new_student.dates.append(struct_time_date)
+
+            if new_student.name is not None:
+                self.students_list.append(new_student)
+
+    def update_sheet(self):
+        for student in self.students_list:
+            # Update name
+            name_cell = self.sheet[str(student.column + str(self.name_row))]
+            name_cell.value = student.name
+            # Update booking
+            booking_cell = self.sheet[student.column + str(self.booking_row)]
+            booking_cell.value = student.booking
+            # Update schedcode
+            schedcode_cell = self.sheet[student.column + str(self.schedcode_row)]
+            for i, cpd in enumerate(student.classes_per_day):
+                student.classes_per_day[i] = str(cpd)
+            schedcode = "s" + "".join(student.classes_per_day)
+            schedcode_cell.value = schedcode
+            # Update dates
+            # Clear old dates
+            for x, row in enumerate(self.sheet.rows):
+                date_cell = self.sheet[student.column + str(self.dates_start_row + x)]
+                date_cell.value = None
+            # Add new dates
+            for x, date in enumerate(student.dates):
+                date_cell = self.sheet[student.column + str(self.dates_start_row + x)]
+                date_cell.value = time.strftime(self.date_format, date)
+        print("Sheet updated.")
+
+    def save_sheet(self):
+        self.workbook.save(self.file_name)
+        print("Workbook saved")
+
+    def print_students(self):
+        for student in self.students_list:
+            print(student.name)
+            print(student.booking)
+            for date in student.dates:
+                print(time.strftime(self.date_format, date))
+
+    @staticmethod
+    def calculate_class_days(student):
+        class_dates_until_today = []
+        if student.classes_per_day is not []:
+            year_day_format = "Year: %Y Day: %j"
+            now = time.localtime()
+            year = now[0]
+            today = now[7] + 1
+            classes_on_day = student.classes_per_day
+            # For every day of the year until today, parse the date.
+            for xday in range(1, today):
+                day_str = "Year: %s Day: %s" % (year, xday)
+                day = time.strptime(day_str, year_day_format)
+                week_day = day[6]
+                # If the student has classes on that date, append the date to the list of dates until today.
+                if classes_on_day[week_day] != 0 and classes_on_day[week_day] != "0":
+                    class_dates_until_today.append(day)
+
+        return class_dates_until_today
---
+++ view_controller.py
@@ -0,0 +1,344 @@
+from tkinter import filedialog, ttk
+import tkinter.messagebox as tkmb
+from tkinter import *
+import model as m
+import calendar
+import string
+import time
+import atexit
+import pickle
+# TODO: Add folder(s) for es.ico, file_path.p, settings data, etc.
+# TODO: Add a delete student button.
+# TODO: Add profile ability so that people sharing the computer can have their own record files.
+# TODO: Add export PayPal invoice (csv) function
+
+
+class MainApplication:
+    def __init__(self, master):
+        atexit.register(self.on_closing)
+        # master.protocol("WM_DELETE_WINDOW", root.iconify)
+        # *** Setup Main Frame ***
+        self.master = master
+
+        main_frame = Frame(master)
+        main_frame.grid(sticky="WENS")
+
+        # Save record file path once selected
+        try:
+            self.file_name = pickle.load(open("file_path.p", "rb"))
+        except FileNotFoundError:
+            self.file_name = filedialog.askopenfilename(
+                initialdir="/", title="Select record file",
+                filetypes=(("Excel files", "*.xls *.xlsx"), ("all files", "*.*"))
+            )
+            self.file_name = self.file_name
+            pickle.dump(self.file_name, open("file_path.p", "wb"))
+        finally:
+            self.model = m.Model(self.file_name)
+
+        self.date_format = self.model.date_format
+
+        # *** Setup Students Listbox ***
+        self.table_frame = Frame(main_frame)
+        self.table_frame.grid(row=1, padx=2, pady=2, sticky="WE")
+        self.students_lb = Listbox(self.table_frame, selectmode=SINGLE, exportselection=False)
+        self.students_lb.grid(row=0, column=0)
+        self.populate_students()
+
+        # *** Setup Info Panel ***
+        self.dates_lb = Listbox(self.table_frame, width=10, exportselection=False)
+        self.dates_lb.grid(row=0, column=1)
+        self.remaining_label = Label(self.table_frame, text="Classes Remaining:")
+        self.remaining_label.grid(row=0, column=2, sticky=N)
+        self.booking_label = Label(self.table_frame, text="N/A", font=("courier", 32), relief=RIDGE)
+        self.booking_label.grid(row=0, column=2)
+
+        # Flip focus from Students_lb to dates_lb with left and right arrow keys.
+        def bind_focus_set(f):
+            f.focus_set()
+        master.bind("<Right>", lambda f=self.dates_lb: bind_focus_set(self.dates_lb))
+        master.bind("<Left>", lambda f=self.dates_lb: bind_focus_set(self.students_lb))
+
+        # ***Setup Toolbar***
+        toolbar = Frame(main_frame, bd=0)
+        toolbar.grid(row=0, sticky=W)
+        new_student_button = Button(toolbar, text="+Student", command=self.add_student_button)
+        new_student_button.grid(row=0, padx=2, pady=2)
+        choose_file_button = Button(toolbar, text="Choose Record File", command=self.open_filedialog)
+        choose_file_button.grid(row=0, column=1, padx=2, pady=2)
+        self.classes_on_day = ttk.Combobox(toolbar, values=list(range(1, 5)), width=1)
+        self.classes_on_day.grid(row=0, column=3, padx=2, pady=2)
+
+        self.had_class_var = IntVar()
+        self.had_class_cb = Checkbutton(toolbar, text="Class done!", fg="green",
+                                        command=self.mark_done, variable=self.had_class_var)
+        self.had_class_cb.grid(row=0, column=2, padx=2, pady=2)
+
+        # ***Setup Menu Bar ***
+        menu = Menu(master)
+        master.config(menu=menu)
+
+        file_menu = Menu(menu, tearoff=0)
+        file_menu.add_command(label="Select Record File", command=self.open_filedialog)
+        file_menu.add_command(label="Refresh Record File", command=self.refresh_record)
+
+        def print_dates():
+            student = self.model.students_list[self.get_student_from_name(self.students_lb.get(ACTIVE))]
+            for date in student.dates:
+                print(date)
+
+        students_menu = Menu(menu, tearoff=0)
+        students_menu.add_command(label="New Student", command=self.add_student_button)
+        students_menu.add_command(label="Edit Student", command=self.edit_student_button)
+        students_menu.add_separator()
+        students_menu.add_command(label="Print Dates", command=print_dates)
+
+        menu.add_cascade(label="File", menu=file_menu)
+        menu.add_cascade(label="Students", menu=students_menu)
+
+        self.populate_dates()
+        self.update_ui()
+
+    def add_student_button(self):
+        AddOrEditStudent("add")
+        try:
+            self.populate_students()
+        except TclError:
+            pass
+
+    def edit_student_button(self):
+        AddOrEditStudent("edit", self.students_lb.get(ACTIVE))
+        try:
+            self.populate_students()
+        except TclError:
+            pass
+
+    def mark_done(self):
+        active_student_name = self.students_lb.get(ACTIVE)
+        active_student_index = self.get_student_from_name(active_student_name)
+        date = self.dates_lb.get(ACTIVE)
+        struct_date = time.strptime(date, self.date_format)
+        how_many_classes = int(self.classes_on_day.get())
+        if self.had_class_var.get() == 0:
+            self.dates_lb.itemconfig(active_student_index, bg="gray", fg="white")
+            for x in range(how_many_classes):
+                self.model.students_list[active_student_index].dates.remove(struct_date)
+            print("Date removed.")
+            self.model.students_list[active_student_index].booking += how_many_classes
+        else:
+            self.dates_lb.itemconfig(active_student_index, bg="#59f766", fg="white")
+            for x in range(how_many_classes):
+                self.model.students_list[active_student_index].dates.append(struct_date)
+            self.model.students_list[active_student_index].booking -= how_many_classes
+            print("Date added.")
+        self.populate_booking(self.model.students_list[active_student_index])
+
+    def get_student_from_name(self, name):
+        for x, student in enumerate(self.model.students_list):
+            if student.name == name:
+                return x
+
+    def populate_booking(self, student):
+        try:
+            value = int(student.booking)
+        except ValueError:
+            value = "N/A"
+
+        if value < 1:
+            self.booking_label.config(text=value, bg="red", bd=5)
+        elif 0 > value < 5:
+            self.booking_label.config(text=value, bg="yellow", bd=5)
+        else:
+            self.booking_label.config(text=value, bg="green", bd=5)
+
+        self.booking_label.config(text=value)
+
+    def populate_dates(self):
+        name = self.students_lb.get(ACTIVE)
+        for student in self.model.students_list:
+            if student.name == name:
+                self.populate_booking(student)
+                self.dates_lb.delete(0, END)
+                # Put the newest date at the top, then put them in the Listbox
+                student.class_dates_until_today = self.model.calculate_class_days(student)
+                dates = list(reversed(student.class_dates_until_today))
+                for date in dates:
+                    self.dates_lb.insert(END, time.strftime(self.date_format, date))
+
+    def populate_students(self):
+        if self.students_lb.size() > 0:
+            self.students_lb.delete(0, END)
+        for student in self.model.students_list:
+            self.students_lb.insert(END, student.name)
+
+    def open_filedialog(self):
+        file_path = filedialog.askopenfilename(
+            initialdir="/", title="Select record file",
+            filetypes=(("Excel files", "*.xls *.xlsx"), ("all files", "*.*"))
+        )
+        self.refresh_record()
+        self.populate_students()
+        return file_path
+
+    def refresh_record(self):
+        if self.file_name is not "":
+            self.model = m.Model(self.file_name)
+
+    def update_ui(self):
+        try:
+            if self.master.focus_get() == self.classes_on_day:
+                pass
+            elif self.master.focus_get() == self.students_lb:
+                self.populate_dates()
+                self.students_lb.select_clear(0, END)
+                self.students_lb.select_set(self.students_lb.index(ACTIVE))
+            elif self.master.focus_get() == self.dates_lb:
+                self.dates_lb.select_clear(0, END)
+                self.dates_lb.select_set(self.dates_lb.index(ACTIVE))
+                active_student_index = self.get_student_from_name(self.students_lb.get(ACTIVE))
+
+                # Make the had_class checkbox reflect whether the student had class on this date or not.
+                active_date_struct = time.strptime(self.dates_lb.get(ACTIVE), self.date_format)
+                current_student_dates = self.model.students_list[active_student_index].dates
+                if active_date_struct in current_student_dates:
+                    self.had_class_var.set(True)
+                else:
+                    self.had_class_var.set(False)
+
+                # Get the number of classes student is supposed to have on this day and put it in the
+                # classes_on_day combobox.
+                self.classes_on_day.set(self.count_classes_on_this_day())
+        except KeyError:
+            # Error while combo box drop down is open. No idea why.
+            print(KeyError)
+
+        # Mark dates green if they exist in the record, white if not
+        index = self.students_lb.index(ACTIVE)
+        for item in [time.strptime(i, self.date_format) for i in self.dates_lb.get(0, END)]:
+            # Turn the date list upside down for user ease. Will be modifiable in settings.
+            reversed_dates_list = enumerate(reversed(list(self.model.students_list[index].class_dates_until_today)))
+            # Get the index of EVERY occurrence of the given date.
+            date_occurrences = [i for i, x in reversed_dates_list if x == item]
+            if item in self.model.students_list[index].dates:
+                for i in date_occurrences:
+                    # Turn them light green with white text
+                    self.dates_lb.itemconfig(i, bg="#59f766", fg="white")
+            else:
+                for i in date_occurrences:
+                    # Turn them gray with white text
+                    self.dates_lb.itemconfig(i, bg="gray", fg="white")
+
+        self.master.after(100, self.update_ui)
+
+    def count_classes_on_this_day(self):
+        # Get the number of classes student is supposed to have on this day
+        active_student_name = self.students_lb.get(ACTIVE)
+        active_student_index = self.get_student_from_name(active_student_name)
+        active_date_struct = time.strptime(self.dates_lb.get(ACTIVE), self.date_format)
+        active_student_cpd = self.model.students_list[active_student_index].classes_per_day
+        classes_on_this_day = active_student_cpd[active_date_struct[6]]
+        return classes_on_this_day
+
+    def flush_changes(self):
+        try:
+            self.model.update_sheet()
+            self.model.save_sheet()
+        except PermissionError:
+            tkmb.showerror("Error", "Please close the record file and try again!")
+        except AttributeError:
+            print("No")
+
+    def on_closing(self):
+        self.flush_changes()
+
+
+class AddOrEditStudent:
+    def __init__(self, action, editee=None):
+        self.window = Tk()
+        self.window.title("Add or Edit Student")
+        self.window.iconbitmap('es.ico')
+        self.frame = Frame(self.window)
+        self.frame.grid(padx=2, pady=2)
+        self.editee_index = None
+        self.action = action
+        self.editee = editee
+
+        self.model = application.model
+
+        self.name_label = Label(self.frame, text="Name")
+        self.name_label.grid(row=0, padx=2, pady=2)
+
+        self.name_entry = Entry(self.frame)
+        self.name_entry.grid(row=0, column=1, columnspan=2, padx=2, pady=2)
+
+        if action == "edit":
+            for student in self.model.students_list:
+                if student.name == editee:
+                    self.editee_index = self.model.students_list.index(student)
+                    self.name_entry.insert(0, editee)
+
+        # Setup a combo box for each day of the week.
+        self.day_cboxes = []
+        for d in range(0, 7):
+            day = Label(self.frame, text=calendar.day_abbr[d])
+            day.grid(row=d + 2)
+            self.cbox = ttk.Combobox(self.frame, width=2, values=list(range(0, 5)))
+            self.day_cboxes.append(self.cbox)
+            self.cbox.grid(row=d + 2, column=1, sticky=W, padx=2, pady=2, ipadx=1, ipady=1)
+            if action == "add":
+                self.cbox.set(0)
+            else:
+                self.cbox.set(self.model.students_list[self.editee_index].classes_per_day[d])
+
+        booking_label = Label(self.frame, text="Booking")
+        booking_label.grid(column=2, row=2, sticky=W)
+
+        self.booking_cbox = ttk.Combobox(self.frame, width=2, values=(10, 25, 50))
+        self.booking_cbox.grid(column=2, row=3, sticky=W, padx=2, pady=2)
+        if action == "add":
+            self.booking_cbox.set(20)
+        else:
+            self.booking_cbox.set(self.model.students_list[self.editee_index].booking)
+
+        save_button = Button(self.frame, text="Save", command=self.save_button)
+        save_button.grid(columnspan=2, padx=2, pady=2, ipadx=1, ipady=1)
+        cancel_button = Button(self.frame, text="Cancel", command=self.my_quit)
+        cancel_button.grid(columnspan=2, padx=2, pady=2, ipadx=1, ipady=1)
+
+        self.window.mainloop()
+
+    def my_quit(self):
+        self.window.quit()
+        self.window.destroy()
+
+    def save_button(self):
+        if self.action == "add":
+            new_student = m.Student()
+            new_student.name = self.name_entry.get()
+            new_student.booking = int(self.booking_cbox.get())
+            new_student.dates = []
+            # TODO: Rework this using row and column notation, where column can be an int instead of a char
+            try:
+                new_student.column = string.ascii_lowercase[len(self.model.students_list) + 1]
+            except IndexError:
+                new_student.column = "A" + string.ascii_lowercase[1]
+            new_student.classes_per_day = [i.get() for i in self.day_cboxes]
+
+            self.model.students_list.append(new_student)
+        else:
+            index = self.editee_index
+            self.model.students_list[index].name = self.name_entry.get()
+            self.model.students_list[index].booking = self.booking_cbox.get()
+            self.model.students_list[index].classes_per_day = [i.get() for i in self.day_cboxes]
+
+        self.my_quit()
+
+
+root = Tk()
+
+root.title("ES Record Helper")
+root.iconbitmap('es.ico')
+root.resizable(False, False)
+application = MainApplication(root)
+
+root.mainloop()
